912ac5f99d8918d825b645639e90668807f0ab0b
Uttejh Reddy <uttejh@gmail.com>
2017-09-14 22:22:12 +0530

    Added extra comments to rest of the code

--- main.py
+++ main.py
@@ -35,18 +35,20 @@ output_dim = 1 # we get only one bit (0 or 1)
 
 # initialize neural network weights
 # SYsnapse represents the network lines (weights)
-synapse_0 = 2*np.random.random((input_dim,hidden_dim)) - 1  # 2 * 16 nets
+synapse_0 = 2*np.random.random((input_dim,hidden_dim)) - 1  # 2 * 16 nets (2 * [0,1] - 1 == (-1,1)))
 synapse_1 = 2*np.random.random((hidden_dim,output_dim)) - 1 # 16 * 1
 synapse_h = 2*np.random.random((hidden_dim,hidden_dim)) - 1	# 16 * 16
 
 # initializing to zeroes
+# fill weight update matrix with zeroes consisting of same dimensions as synapse_0,1,h
 synapse_0_update = np.zeros_like(synapse_0) 
 synapse_1_update = np.zeros_like(synapse_1)
 synapse_h_update = np.zeros_like(synapse_h)
 
 # training logic
-#10000 traing iterations
-for j in range(10000):
+# 10000 traing iterations
+# The loop ends at the last line of the code
+for j in range(100000):
     
     # generate a simple addition problem (a + b = c)
     # diving by 2 so that (a [max 128] + b [max 128] = c [not greater than 256(our greatest no.)]) 
@@ -85,33 +87,47 @@ for j in range(10000):
         y = np.array([[c[binary_dim - position - 1]]]).T
 
         # hidden layer (input ~+ prev_hidden)
-        # dot product between inputs (layer 0) and initial weights + dot product of layer 1 and hidden layer weights
+        # dot product between inputs (layer 0) and initial weights + dot product of current hidden layer and previous hidden layer weights
+        # since a hidden layer is constructed based on present inputs and data from previous hidden layer
+        # layer_1_values consists of values in form of an array in a list. layer_1_values[-1] == array (only),layer_1_values == list 
         layer_1 = sigmoid(np.dot(X,synapse_0) + np.dot(layer_1_values[-1],synapse_h))
 
         # output layer (new binary representation)
         # output = dot of l1 and s1
+        # 1*1 matrix is produced (1*8).(16*1)
         layer_2 = sigmoid(np.dot(layer_1,synapse_1))
 
         # did we miss?... if so, by how much?
         # error = binary difference of desired output and generated output
         layer_2_error = y - layer_2
+        # layer 2 derivative. Consists of error diff at the output layer.
         layer_2_deltas.append((layer_2_error)*sigmoid_output_to_derivative(layer_2))
+        # layer_2_error[0] == array (i guess)
         overallError += np.abs(layer_2_error[0])
     
         # decode estimate so we can print it out
+        # layer_2[0][0] = exact value
+        # d stores what the system predicted (value of a+b). It predicts ans as 0.33,etc.. We take it as 0.(0.63 ==1)
         d[binary_dim - position - 1] = np.round(layer_2[0][0])
         
         # store hidden layer so we can use it in the next timestep
+        # hidden layer values (this becomes previous values in next iteration)
         layer_1_values.append(copy.deepcopy(layer_1))
     
+    # init to zero
     future_layer_1_delta = np.zeros(hidden_dim)
-    
+
+	# let's backpropogate from layer_2 to layer_0    
     for position in range(binary_dim):
         
         X = np.array([[a[position],b[position]]])
+
+        # The only current layer value is the last element (las neuron) in the hidden layer
+        # rest all neurons iherit values from previous hidden layer (as shown in the figure)
         layer_1 = layer_1_values[-position-1]
+        # leaving the last element. Starting from last but one
         prev_layer_1 = layer_1_values[-position-2]
-        
+
         # error at output layer
         layer_2_delta = layer_2_deltas[-position-1]
         # error at hidden layer
@@ -124,29 +140,26 @@ for j in range(10000):
         
         future_layer_1_delta = layer_1_delta
     
-
+    # multiplying and summing up the final update each iteration out of 100000 with learning rate
     synapse_0 += synapse_0_update * alpha
     synapse_1 += synapse_1_update * alpha
     synapse_h += synapse_h_update * alpha    
 
+    # re-initializing
     synapse_0_update *= 0
     synapse_1_update *= 0
     synapse_h_update *= 0
     
-    # print out progress
+    # print out progress after 1000 iterations
     if(j % 1000 == 0):
-        print ("Error:" ),
-        print(str(overallError))
-        print ("Pred:"),
-        print(str(d))
-        print("True:")
-        print( str(c))
+        print "Error:" + str(overallError)
+        print "Pred:" + str(d)
+        print "True:" + str(c)
         out = 0
         for index,x in enumerate(reversed(d)):
             out += x*pow(2,index)
-        print(str(a_int)),
-        print(" + " ),
-        print(str(b_int)),
-        print(" = " ),
-        print(str(out))
-        print ("------------")
+        print str(a_int) + " + " + str(b_int) + " = " + str(out)
+        print "------------"
+
+        
+
